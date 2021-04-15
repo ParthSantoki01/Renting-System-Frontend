9038c56aa7d666e06a72b95f34945f228cdafd17
prit29 <pritsahkar2000@gmail.com>
2021-04-15 15:56:16 +0530

    Address Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { NOT_LOGIN, BUYER_LOGIN, SELLER_LOGIN } from './Assets/Constant';
 import Drawer from './Components/Drawer/Drawer';
+import BuyerAddresses from './Pages/Buyer/BuyerAddresses';
 import Dashboard from './Pages/General/Dashboard';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
                 <Drawer page={NOT_LOGIN} />
             </div>
             <div className='Main-body'>
-                <Dashboard />
+                <BuyerAddresses />
             </div>
         </div>
     );
