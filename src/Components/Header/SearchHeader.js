6f41c357d9450704d72b3d4e9fccea3602b444bd
Parth Santoki <parthsantoki2362@gmail.com>
2021-04-16 16:36:17 +0530

    Add Buyer Profile Page

--- src/Components/Header/SearchHeader.js
+++ src/Components/Header/SearchHeader.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
 import './style.css';
 import { Icon } from '@iconify/react';
-import layersSearch from '@iconify-icons/mdi/layers-search';
+import magnifyIcon from '@iconify-icons/mdi/magnify';
 
 const SearchHeader = () => {
-    const [value, setValue] = useState('');
+  const [value, setValue] = useState('');
 
-    const handleInputChanges = (event) => {
-        setValue(event.target.value);
-    };
+  const handleInputChanges = (event) => {
+    setValue(event.target.value);
+  };
 
-    const handleOnClick = () => {
-        console.log(value);
-        setValue('');
-    };
+  const handleOnClick = () => {
+    console.log(value);
+    setValue('');
+  };
 
-    return (
-        <div className='SearchHeader'>
-            <div className='SearchHeader-body'>
-                <input
-                    type='text'
-                    value={value}
-                    onChange={handleInputChanges}
-                    placeholder='Search'
-                    className='SearchHeader-search'
-                />
-                <Icon
-                    icon={layersSearch}
-                    onClick={handleOnClick}
-                    className='SearchHeader-search-icon'
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className='SearchHeader'>
+      <div className='SearchHeader-body'>
+        <input
+          type='text'
+          value={value}
+          onChange={handleInputChanges}
+          placeholder='Search'
+          className='SearchHeader-search'
+        />
+        <Icon
+          icon={magnifyIcon}
+          onClick={handleOnClick}
+          className='SearchHeader-search-icon'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchHeader;
--- src/Pages/Buyer/BuyerProfile.js
+++ src/Pages/Buyer/BuyerProfile.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import BuyerMyOrder from './BuyerMyOrder';
+import TitleHeader from '../../Components/Header/TitleHeader';
+import logo from '../../Assets/logo512.png';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-const BuyerProfile = () => {
-    return <div>Buyer Profile</div>;
+import cardAccountDetails from '@iconify-icons/mdi/card-account-details';
+import arrowLeftCircle from '@iconify-icons/mdi/arrow-left-circle';
+import shoppingIcon from '@iconify-icons/mdi/shopping';
+
+const BuyerProfile = (props) => {
+  return (
+    <div className='BuyerProfile-page'>
+      <TitleHeader name={'My Profile'} />
+      <div id='main'>
+        <div className='BuyerProfile-main'>
+          <div className='BuyerProfile-div'>
+            <Icon icon={cardAccountDetails} className='BuyerProfile-image' />
+            <div className='BuyerProfile-title'>Personal Information</div>
+          </div>
+          <div className='BuyerProfile-sub'>
+            <div className='BuyerProfile-namediv'>
+              <div className='BuyerProfile-hello'> Hello, </div>
+              <div className='BuyerProfile-name'> {props.name}</div>
+            </div>
+            <div className='BuyerProfile-details'>
+              <div className='BuyerProfile-addressdiv'>
+                <div className='BuyerProfile-dis'>Address</div>
+                <div>{props.address}</div>
+              </div>
+              <div className='BuyerProfile-mobilenumberdiv'>
+                <div className='BuyerProfile-dis'>Mobile Number</div>
+                <div>{props.mobilenumber}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link to='./order'>
+          <div className='BuyerProfile-div'>
+            <Icon icon={shoppingIcon} className='BuyerProfile-image' />
+            <div className='BuyerProfile-title'>My Orders</div>
+          </div>
+        </Link>
+        <Link to='./signout'>
+          <div className='BuyerProfile-div'>
+            <Icon icon={arrowLeftCircle} className='BuyerProfile-image' />
+            <div className='BuyerProfile-title'>SignOut</div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+BuyerProfile.defaultProps = {
+  name: 'Parth',
+  address: 'Dhrol , Gujarat',
+  mobilenumber: 6353812362,
 };
 
 export default BuyerProfile;
