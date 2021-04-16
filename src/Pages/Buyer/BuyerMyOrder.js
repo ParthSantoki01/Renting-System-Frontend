3cbe43b8dd1bb201dd108ff54438dd4be78279f7
rittu6820 <rittu6820@gmail.com>
2021-04-16 20:50:44 +0530

    Buyer Wishlist and General Cardview

--- src/Pages/Buyer/BuyerMyOrder.js
+++ src/Pages/Buyer/BuyerMyOrder.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from 'react'
+import GeneralCard from '../../Components/Cardview/GeneralCard';
+
 
 const BuyerMyOrder = () => {
-    return <div>Buyer My Order</div>;
-};
+   
+        const cards = [];
+        for (var i = 0; i < 5; i++) {
+            cards.push(<GeneralCard key={i} />);
+        }
+        return (
+            <div className='main-body' >
+                <div className='Main-card'>{cards}</div>
+            </div>
+        )
+    
+}
 
-export default BuyerMyOrder;
+export default BuyerMyOrder
