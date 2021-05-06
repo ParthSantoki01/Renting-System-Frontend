b04fea5a5927b180b54b2e0a912154ddcc9f2cf2
Parth Santoki <parthsantoki2362@gmail.com>
2021-05-07 01:10:13 +0530

    Conflict Solve for Post order API

--- src/Pages/Buyer/BuyerCheckout.js
+++ src/Pages/Buyer/BuyerCheckout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style.css';
 import { Link, useLocation } from 'react-router-dom';
@@ -11,60 +11,74 @@ const BuyerCheckout = (props) => {
   let location = useLocation();
   // console.log(location.state);
 
+  const [check, setCheck] = useState(false);
+  const [Buyer, setData] = useState({});
+
+  useEffect(() => {
+    const fetch = () => {
+      axios
+        .get('https://rentingsystem.herokuapp.com/buyer/detail', {
+          headers: {
+            auth_token: localStorage.getItem('auth_token'),
+          },
+        })
+        .then((response) => {
+          setData(response.data.buyer[0]);
+          // console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+
+    fetch();
+  }, []);
+
   const confirmpaymant = () => {
+    let returndate = new Date();
+    let k = 0;
+    if (location.state.formatofprice === '/day') {
+      k = 1;
+    } else if (location.state.formatofprice === '/3-days') {
+      k = 3;
+    } else if (location.state.formatofprice === '/week') {
+      k = 7;
+    } else if (location.state.formatofprice === '/15-days') {
+      k = 15;
+    } else if (location.state.formatofprice === '/month') {
+      k = 30;
+    } else if (location.state.formatofprice === '/6-months') {
+      k = 182;
+    } else if (location.state.formatofprice === '/year') {
+      k = 365;
+    }
+
+    returndate.setDate(returndate.getDate() + k);
     axios
-      .get('https://rentingsystem.herokuapp.com/buyer/detail', {
-        headers: {
-          auth_token: localStorage.getItem('auth_token'),
-        },
+      .post('https://rentingsystem.herokuapp.com/order', {
+        buyerid: Buyer._id,
+        sellerid: location.state.seller,
+        productid: location.state._id,
+        address: Buyer.address,
+        totalprice: location.state.price,
+        returndate: returndate,
       })
       .then((response) => {
-        let returndate = new Date();
-        let k = 0;
-        if (location.state.formatofprice == '/day') {
-          k = 1;
-        } else if (location.state.formatofprice == '/3-days') {
-          k = 3;
-        } else if (location.state.formatofprice == '/week') {
-          k = 7;
-        } else if (location.state.formatofprice == '/15-days') {
-          k = 15;
-        } else if (location.state.formatofprice == '/month') {
-          k = 30;
-        } else if (location.state.formatofprice == '/6-months') {
-          k = 182;
-        } else if (location.state.formatofprice == '/year') {
-          k = 365;
+        // console.log(response);
+        if (response.error) {
+          alert.error(response.data.msg);
+        } else {
+          alert.success(response.data.msg);
         }
-
-        returndate.setDate(returndate.getDate() + k);
-
-        axios
-          .post('https://rentingsystem.herokuapp.com/order', {
-            buyerid: response.data.buyer[0]._id,
-            sellerid: location.state.seller,
-            productid: location.state._id,
-            address: response.data.buyer[0].address,
-            totalprice: location.state.price,
-            returndate: returndate,
-          })
-          .then((response) => {
-            console.log(response);
-            if (response.error) {
-              alert.error(response.data.msg);
-            } else {
-              alert.success(response.data.msg);
-            }
-          })
-          .catch((e) => {
-            console.log(e);
-          });
       })
       .catch((e) => {
         console.log(e);
       });
   };
 
+  const handlechange = () => {
+    setCheck(!check);
+  };
   return (
     <div>
       <TitleHeader name={'Checkout Product'} />
@@ -80,13 +94,12 @@ const BuyerCheckout = (props) => {
         <hr />
         <div className='BuyerCheckout-address'>
           <div className='BuyerCheckout-add'>Address</div>
-          <div className='BuyerCheckout-Buyeraddress'>
-            {location.state.address}
-          </div>
+          <div className='BuyerCheckout-Buyeraddress'>{Buyer.address}</div>
         </div>
         <hr />
         <div className='BuyerCheckout-confirmdiv'>
           <input
+            onChange={handlechange}
             type='checkbox'
             className='BuyerCheckout-check'
             defaultChecked={false}
@@ -107,11 +120,13 @@ const BuyerCheckout = (props) => {
             </Link>
           </div>
           <div className='BuyerCheckout-button BuyerCheckout-placeorder'>
-            <Button
-              icon={checkboxMarkedCircleOutline}
-              name={'Place Order'}
-              handleClick={confirmpaymant}
-            />
+            {check && (
+              <Button
+                icon={checkboxMarkedCircleOutline}
+                name={'Place Order'}
+                handleClick={confirmpaymant}
+              />
+            )}
           </div>
         </div>
       </div>
